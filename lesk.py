c2b25bc9c6352908c7a3fcd9c733460d9d17fba3
Keval Mehta <kevalmehta885@gmail.com>
2025-12-16 06:48:35 +0530

    lest

--- lesk.py
+++ lesk.py
@@ -17,3 +17,44 @@ if sense:
     print("Definition:", sense.definition())
 else:
     print("No sense found")
+
+
+# OR code using manual implementation of Lesk Algorithm
+
+# import nltk
+# nltk.download('wordnet')
+# nltk.download('omw-1.4')
+# nltk.download('punkt')
+
+# from nltk.corpus import wordnet as wn
+# from nltk.tokenize import word_tokenize
+
+# def lesk_algorithm(sentence, target_word):
+#     # Tokenize and lowercase context
+#     context = set(word_tokenize(sentence.lower()))
+    
+#     max_overlap = 0
+#     best_sense = None
+
+#     # Iterate through all possible senses
+#     for sense in wn.synsets(target_word):
+#         # Tokenize definition (gloss)
+#         gloss = set(word_tokenize(sense.definition().lower()))
+
+#         # Calculate overlap
+#         overlap = len(context.intersection(gloss))
+
+#         if overlap > max_overlap:
+#             max_overlap = overlap
+#             best_sense = sense
+
+#     return best_sense
+
+# sentence = "He went to the bank to deposit money"
+# target_word = "bank"
+
+# sense = lesk_algorithm(sentence, target_word)
+# print("Best Sense:", sense)
+# print("Definition:", sense.definition())
+
+
